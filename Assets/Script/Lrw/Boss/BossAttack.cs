3ac59efcb09ecaf6c4074b66eb5415c986b7c54c
riwoo0902 <imliu4342@gmail.com>
2025-09-26 11:06:11 +0900

    save

---
+++ Assets/Script/Lrw/Boss/BossAttack.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Lrw_Boss
+{
+    public class BossAttack : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
