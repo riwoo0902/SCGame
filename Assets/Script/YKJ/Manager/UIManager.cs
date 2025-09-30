d4a2e32a58a30203ff1c3fea693b0602ca2cdbef
didrudwns <didrudwns123@gmail.com>
2025-09-30 11:42:50 +0900

    feat:BackGround&&Menu

--- Assets/Script/YKJ/Manager/UIManager.cs
+++ Assets/Script/YKJ/Manager/UIManager.cs
@@ -2,8 +2,17 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+
+    [SerializeField] GameObject MenuWindow;
     public void LodeScene()
     {
 
+    }
+    public void SetSetting()
+    {
+        if (MenuWindow.activeSelf == false)
+            MenuWindow.SetActive(true);
+        else
+            MenuWindow.SetActive(false);
     }
 }
