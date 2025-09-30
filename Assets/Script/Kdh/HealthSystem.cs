cf9e5dc1c7a88a108b64c71d8fbe68180e1936f6
김도희 <dohuiasdf@gmail.com>
2025-09-30 11:42:30 +0900

    feat : HealthSystem

---
+++ Assets/Script/Kdh/HealthSystem.cs
@@ -0,0 +1,46 @@
+using System;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    [SerializeField] private int maxHealth = 100;
+    private int minHealth = 0;
+
+    public int currentHealth { get; private set; }
+    public bool IsDead { get; private set; }
+
+    public event Action<int, int> OnHealthChanged; // (현재 체력, 최대 체력)
+    public event Action OnDeath;
+
+    private void Awake()
+    {
+        
+    }
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        IsDead = false;
+        OnHealthChanged?.Invoke(currentHealth, maxHealth);
+    }
+
+    public void Deal(int damage)
+    {
+        currentHealth = Mathf.Max(currentHealth - damage, minHealth);
+
+        if (currentHealth == minHealth)
+        {
+            Death();
+        }
+    }
+
+    public void Heal(int healAmount)
+    {
+        currentHealth = Mathf.Min(currentHealth + healAmount, maxHealth);
+    }
+
+    public void Death()
+    {
+        Debug.Log("죽음");
+    }
+
+}
