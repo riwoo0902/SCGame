43fe1e24a508a24856246d7301f8d38ee9f6628a
didrudwns <didrudwns123@gmail.com>
2025-10-12 22:05:01 +0900

    feat:LobbyUI

--- Assets/Script/YKJ/Manager/UIManager.cs
+++ Assets/Script/YKJ/Manager/UIManager.cs
@@ -1,18 +1,21 @@
 using UnityEngine;
-
+using DG.Tweening;
 public class UIManager : MonoBehaviour
 {
 
     [SerializeField] GameObject MenuWindow;
+    private bool IsMenu = false;
     public void LodeScene()
     {
 
     }
     public void SetSetting()
     {
-        if (MenuWindow.activeSelf == false)
-            MenuWindow.SetActive(true);
+        if (IsMenu == false)
+            MenuWindow.GetComponent<RectTransform>().DOAnchorPosX(0, 0.3f);
         else
-            MenuWindow.SetActive(false);
+            MenuWindow.GetComponent<RectTransform>().DOAnchorPosX(600, 0.3f);
+        IsMenu = !IsMenu;
     }
+    public void ExitGame() => Application.Quit();
 }
