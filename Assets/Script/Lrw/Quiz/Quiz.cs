6987f745709466f5b7df761749fc232229c3545e
riwoo0902 <imliu4342@gmail.com>
2025-10-12 23:06:29 +0900

    save

--- Assets/Script/Lrw/Quiz/Quiz.cs
+++ Assets/Script/Lrw/Quiz/Quiz.cs
@@ -4,15 +4,22 @@ namespace Lrw_Quiz
 {
     public class Quiz : MonoBehaviour
     {
-        private QuizButton[] _quizButtons;
+        private QuizButton O_QuizButton;
+        private QuizButton X_QuizButton;
+
         [SerializeField] private Transform checkTarget;
         [field: SerializeField] public bool O_Button { get; private set; } = false;
         [field: SerializeField] public bool X_Button { get; private set; } = false;
         private void Awake()
         {
-            _quizButtons = GetComponentsInChildren<QuizButton>();
-            foreach (QuizButton i in _quizButtons) i.SetLayerMask(checkTarget);
-            
+            QuizButton[] _quizButtons = GetComponentsInChildren<QuizButton>();
+            O_QuizButton = _quizButtons[0];
+            O_QuizButton.SetLayerMask(checkTarget);
+            X_QuizButton = _quizButtons[1];
+            X_QuizButton.SetLayerMask(checkTarget);
+
+
+
         }
 
 
