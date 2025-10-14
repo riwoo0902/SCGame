89174a1725cf776f3d6497b9d3a3215ceb1f909a
김도희 <dohuiasdf@gmail.com>
2025-10-14 11:30:02 +0900

    feat : UI

--- Assets/Script/Kdh/Pause.cs
+++ Assets/Script/Kdh/Pause.cs
@@ -7,10 +7,14 @@ using UnityEngine.InputSystem;
 public class Pause : MonoBehaviour
 {
     [SerializeField] private Image pausePanel;
+    [SerializeField] private Button startBtn;
+    [SerializeField] private Button endBtn;
 
     private void Start()
     {
         pausePanel.gameObject.SetActive(false);
+        startBtn.gameObject.SetActive(false);
+        endBtn.gameObject.SetActive(false);
     }
 
     private void Update()
@@ -18,7 +22,26 @@ public class Pause : MonoBehaviour
         if (Keyboard.current.escapeKey.wasPressedThisFrame)
         {
             pausePanel.gameObject.SetActive(true);
+            startBtn.gameObject.SetActive(true);
+            endBtn.gameObject.SetActive(true);
             Time.timeScale = 0;
         }
+    }
+
+    public void StartBtn()
+    {
+        pausePanel.gameObject.SetActive(false);
+        startBtn.gameObject.SetActive(false);
+        endBtn.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void EndBtn()
+    {
+        pausePanel.gameObject.SetActive(false);
+        startBtn.gameObject.SetActive(false);
+        endBtn.gameObject.SetActive(false);
+        Application.Quit();
+        
     }
 }
