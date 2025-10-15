257ea331e349a0c96ac31fa943a20c0ea452b026
riwoo0902 <imliu4342@gmail.com>
2025-10-15 22:41:21 +0900

    save

--- Assets/Script/Lrw/Boss/BossState/BossAttack1.cs
+++ Assets/Script/Lrw/Boss/BossState/BossAttack1.cs
@@ -1,9 +1,12 @@
+using System.Collections;
 using UnityEngine;
+
 
 namespace Lrw_Boss
 {
     public class BossAttack1 : BossState
     {
+        private float currentTime = 0;
         public BossAttack1(Boss boss) : base(boss)
         {
             
@@ -11,12 +14,24 @@ namespace Lrw_Boss
 
         public override void Enter()
         {
-            
+            _boss.StartCoroutine(EndWait());
+        }
+
+        private IEnumerator EndWait()
+        {
+            yield return new WaitForSeconds(5);
+            _boss._bossBrain.ChangeBossState(BossStates.Idle);
         }
 
         public override void Update()
         {
-            Debug.Log("°ø°ÝÁß");
+            currentTime -= Time.deltaTime;
+            if (currentTime <= 0)
+            {
+                currentTime += 0.15f;
+                Object.Instantiate(_boss.BossAttackBulletPrefab).transform.position = new Vector3(UnityEngine.Random.Range(-6f, 6f), 1, 0);
+
+            }
         }
 
         public override void Exit()
--- Assets/Script/Lrw/Boss/BossState/BossIdle.cs
+++ Assets/Script/Lrw/Boss/BossState/BossIdle.cs
@@ -19,16 +19,16 @@ namespace Lrw_Boss
         private IEnumerator ChangeState()
         {
             yield return new WaitForSeconds(5f);
-            _boss._bossBrain.ChangeBossState(BossStates.Quiz);
-            //int randomint = Random.Range(0,4);
-            //if (randomint == 0)
-            //{
-            //    _boss._bossBrain.ChangeBossState(BossStates.Hide);
-            //}
-            //else if (randomint == 1)
-            //{
-            //    _boss._bossBrain.ChangeBossState(BossStates.Attack1);
-            //}
+            
+            int randomint = Random.Range(0,2);
+            if (randomint == 0)
+            {
+                _boss._bossBrain.ChangeBossState(BossStates.Quiz);
+            }
+            else if (randomint == 1)
+            {
+                _boss._bossBrain.ChangeBossState(BossStates.Attack1);
+            }
             //else if (randomint == 2)
             //{
             //    _boss._bossBrain.ChangeBossState(BossStates.Attack2);
