dc05e85eaefddee0fed563a91fdcaba6634f118a
didrudwns <didrudwns123@gmail.com>
2025-10-15 22:09:00 +0900

    feat:Font And UI

--- Assets/Script/YKJ/Manager/UIManager.cs
+++ Assets/Script/YKJ/Manager/UIManager.cs
@@ -2,20 +2,10 @@ using UnityEngine;
 using DG.Tweening;
 public class UIManager : MonoBehaviour
 {
-
-    [SerializeField] GameObject MenuWindow;
     private bool IsMenu = false;
     public void LodeScene()
     {
 
-    }
-    public void SetSetting()
-    {
-        if (IsMenu == false)
-            MenuWindow.GetComponent<RectTransform>().DOAnchorPosX(0, 0.3f);
-        else
-            MenuWindow.GetComponent<RectTransform>().DOAnchorPosX(600, 0.3f);
-        IsMenu = !IsMenu;
     }
     public void ExitGame() => Application.Quit();
 }
